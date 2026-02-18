139f8ba709736f89c373c77308ba2d61bfa880fd
mihirvt <mihirvt@gmail.com>
2026-02-18 21:35:18 +0530

    Vendor pipecat library to fix deployment submodule errors

---
+++ pipecat/src/pipecat/utils/enums.py
@@ -0,0 +1,20 @@
+"""Enumeration types for pipecat utilities."""
+
+from enum import Enum
+
+
+class EndTaskReason(Enum):
+    """Reasons for ending a task."""
+
+    CALL_DURATION_EXCEEDED = "call_duration_exceeded"
+    CALL_TRANSFERRED = "call_transferred"
+    END_CALL_TOOL_REASON = "end_call_tool"
+    VOICEMAIL_DETECTED = "voicemail_detected"
+    USER_IDLE_MAX_DURATION_EXCEEDED = "user_idle_max_duration_exceeded"
+    USER_HANGUP = "user_hangup"
+    USER_QUALIFIED = "user_qualified"
+    USER_DISQUALIFIED = "user_disqualified"
+    SYSTEM_CANCELLED = "system_cancelled"
+    SYSTEM_CONNECT_ERROR = "system_connect_error"
+    UNEXPECTED_ERROR = "unexpected_error"
+    UNKNOWN = "unknown"
